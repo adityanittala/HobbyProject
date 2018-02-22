e930d3ed4cfe4f953bec54960e4f5ba9299bef17
adityanittala <aditya.nittala@gmail.com>
2018-02-22 07:08:48 -0500

    Add files via upload

---
+++ simplifiedTool/js/layerRenderer.js
@@ -0,0 +1,14 @@
+﻿function renderRXLayer(rxLayerDraw) {
+
+
+    var ellipse = rxLayerDraw.ellipse(200, 100)
+
+}
+
+function renderTXLayer(txLayerDraw) {
+
+}
+
+function renderDielectric(dielectricLayerDraw) {
+    var ell = dielectricLayerDraw.ellipse(200, 100);
+}
